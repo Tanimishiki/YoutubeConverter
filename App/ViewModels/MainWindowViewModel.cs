b8c0b7a4fd3c9e2a9b8e8a0df395451a5bb3c596
Tanimishiki <60542682+Tanimishiki@users.noreply.github.com>
2024-10-16 01:26:01 +0800

    Fixed app crashing when downloading (403 error code).

--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,6 @@
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Title => "Youtube Converter (1.0.0)";
+    public string Title => "Youtube Converter (1.0.1)";
 #pragma warning restore CA1822 // Mark members as static
 }
